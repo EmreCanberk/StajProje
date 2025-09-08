a27779b629f00a123a66fc95fe9e74bff5fcb573
Emre Canberk Tok <149680770+EmreCanberk@users.noreply.github.com>
2025-09-09 02:44:21 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
-// src/main.js
+
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; // Router'ı içeri aktar
+import router from './router';
 
-// Firebase servislerini içeri aktar
-// eslint-disable-next-line no-unused-vars
-import { auth, db } from './firebaseConfig'; // db'yi de ekledik
 
-// onAuthStateChanged'ı içeri aktar
+import { auth, db } from './firebaseConfig'; 
+
 import { onAuthStateChanged } from 'firebase/auth';
 
 const app = createApp(App);
 
-app.use(router); // Vue Router'ı kullan
+app.use(router); 
 
-// Firebase Auth durumunu dinleyerek ilk rota yönlendirmesini yap
-// Bu, sayfa yenilendiğinde kimlik doğrulama durumunun doğru şekilde ele alınmasını sağlar
+
 let appInitialized = false;
 onAuthStateChanged(auth, () => {
   if (!appInitialized) {
